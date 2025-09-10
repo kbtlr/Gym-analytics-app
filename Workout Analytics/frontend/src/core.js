c12165dfa76a1db7984b4adb65af94fbb658b234
kbtlr <ky.butler1210@gmail.com>
2025-09-11 00:35:48 +0100

    skeleton files added

---
+++ Workout Analytics/frontend/src/core.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Workout Analytics</h1>
+            <p>Welcome to your workout analytics dashboard!</p>
+        </div>
+    );
+}
+
+export default App;

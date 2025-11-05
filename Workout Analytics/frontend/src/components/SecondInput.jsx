123340ec8b79c8203c766be18f8852c9234355bd
kbtlr <ky.butler1210@gmail.com>
2025-11-05 22:22:17 +0000

    skeleton files

---
+++ Workout Analytics/frontend/src/components/SecondInput.jsx
@@ -0,0 +1,9 @@
+function SecondInput({ onNext }) {
+  return (
+    <div className="-">
+     //Establish training period, goal and PR's of big three
+    </div>
+  );
+}
+
+export default WelcomeScreen;

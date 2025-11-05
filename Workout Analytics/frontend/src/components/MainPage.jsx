17f7b205870f0a1f4f02d6a3bbe2e12dfbe0dd2b
kbtlr <ky.butler1210@gmail.com>
2025-11-05 22:22:17 +0000

    skeleton files

---
+++ Workout Analytics/frontend/src/components/MainPage.jsx
@@ -0,0 +1,9 @@
+function MainPage({ onNext }) {
+  return (
+    <div className="main-page">
+      //Display data graphically over the alotted period
+    </div>
+  );
+}
+
+export default WelcomeScreen;

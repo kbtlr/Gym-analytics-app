369bf7da527222fef4206e680943fa742aa0be97
kbtlr <ky.butler1210@gmail.com>
2026-02-24 21:24:09 +0000

    html/js: better info page, first look at graph and backend

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 //js file to sort interactions involving click behaviour, UI changes,
 // and data fetching for the web application
-
 document.addEventListener('DOMContentLoaded', function() {
     // Initialize UI components
     initUIComponents();});
@@ -17,3 +16,19 @@ function newPage(shown, hidden) {
     document.getElementById(hidden).style.display='none';
     return false;
 }
+var char = new CanvasJS.Chart("myChart", {
+    animationEnabled: true,
+    theme: "light2",
+    title:{
+        text: "Volume by Week"
+    },
+    data: [{
+        type: "column",
+        indexLabelFontSize: 16,
+        dataPoints: [  
+            { label: "Week 1", y: 10000 },
+            { label: "Week 2", y: 12000 },
+            { label: "Week 3", y: 15000 },
+        ]
+    }]
+});
